af93cf7fda8137b239d3dbe281c102b47cc578be
(SSB-12D9) Vuong.Nguyen <vuong.nguyen@ssbshoes.com>
2021-12-10 09:00:01 +0700

    add prime number challenge

--- main.ts
+++ main.ts
@@ -1,4 +1,24 @@
 /* -------------------------------------------------------------------------- */
-/*                   tsnd --respawn --quiet --clear main.ts                   */
-/* -------------------------------------------------------------------------- */
 
+/**
+ * Input: [13, 67, 47, 83, 97]
+ * Output: true
+ * 
+ * Input: [34, 71, 29, 93, 70]
+ * Output: false
+ * 
+ * Input: []
+ * Output: false
+ */
+
+let isPrimeNumber = (num: number): boolean =>
+  num >= 2 && ![...Array(Math.floor(Math.sqrt(num)))].map((_, i) => i + 2).slice(0, -1).some(i => num % i === 0);
+
+let checkAllPrimeNumbers = (arr: number[]): boolean =>
+  arr.length !== 0 && arr.every(i => isPrimeNumber(i));
+
+// console.log(checkAllPrimeNumbers([13, 67, 47, 83, 97]));
+// console.log(checkAllPrimeNumbers([34, 71, 29, 93, 70]));
+// console.log(checkAllPrimeNumbers([]));
+
+/* -------------------------------------------------------------------------- */
